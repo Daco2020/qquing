19734be03b36241b48caf723a1257bd1503ba221
Daco2020 <dacokim32@gmail.com>
2023-12-16 01:48:51 +0900

    build: cloudflare 로 배포하기 위해 빌드 출력 디렉토리를 'build'로 설정

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build', // cloudflare 로 배포하기 위해 빌드 출력 디렉토리를 'build'로 설정
+  },
 })
