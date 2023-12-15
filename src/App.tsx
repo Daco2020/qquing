24c81e3e6beec7078e8d5bb5fa1e1d3aa259c2bd
Daco2020 <dacokim32@gmail.com>
2023-12-16 01:36:40 +0900

    feat: 통통 튀는 박스

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
 import './App.css'
-import Hello from './Hello.tsx'
-import ThreeElement from './ThreeElement.tsx'
 import { Canvas } from '@react-three/fiber'
+import ThreeElement from './ThreeElement.tsx'
+import { OrbitControls } from '@react-three/drei'
+import { useControls } from 'leva'
 
 function App() {
+
+  const controls = useControls({
+    color: '#ffffff',
+    segment: {value:20, min:10, max:500, step:1}
+  })
   
   return (
     <>
     <Canvas>
+      <color attach="background" args={[controls.color]}/>
+      <OrbitControls/>
+      <axesHelper args={[50]}/>
+      <gridHelper args={[controls.segment, controls.segment, "#d1d1d1", "#d1d1d1"]}/>
       <ThreeElement/>
     </Canvas>
-      <Hello to="QQuing" from="Daco"></Hello>
     </>
   )
 }
--- src/ThreeElement.tsx
+++ src/ThreeElement.tsx
@@ -1,13 +1,56 @@
 import * as THREE from 'three'
+import {useControls} from 'leva'
+import { useFrame } from '@react-three/fiber'
+import { useRef, useState } from 'react'
 
 export default function ThreeElement() {
+  // useRef는 개체를 참조하는데 사용되는 훅
+  const boxRef = useRef<THREE.Mesh>(null!)
+  const [velocity, setVelocity] = useState(0);
+  const [direction, setDirection] = useState(new THREE.Vector3());
+
+
+  // 개체를 움직이게 만드는 곳
+  useFrame((state, delta) => {
+    // 중력 효과
+    let newVelocity = velocity - 12.8 * delta; // 중력 가속도 적용
+    boxRef.current.position.y += newVelocity * delta;
+    boxRef.current.position.x += direction.x * delta;
+    boxRef.current.position.z += direction.z * delta;
+
+    if (boxRef.current.position.y <= 0) {
+      // 바닥에 닿으면
+      boxRef.current.position.y = 0;
+      newVelocity *= -0.5; // 반발력 적용
+
+      if (Math.abs(newVelocity) < 0.5) {
+        newVelocity = 0; // 최소 속도 도달 시 정지
+        setDirection(new THREE.Vector3(0,0,0)); // 정지 시 방향도 0으로 설정
+    }
+  }
+    setVelocity(newVelocity);
+  });
+
+  const handleClick = () => {
+    setVelocity(5); // 초기 속도 설정
+    setDirection(new THREE.Vector3(Math.random() * 2 - 1, 0, Math.random() * 2 - 1).normalize()); // 랜덤 방향 설정
+  };
+
+  const controls = useControls({
+    xRotation: {value:0, min:-360, max:360, step:1},
+    yRotation: {value:0, min:-360, max:360, step:1}
+  })
+
   return (
     <>
       <directionalLight position={[1,5,5]}/>
-      {/* <mesh rotation={[0,30,0]}> */}
-      <mesh rotation={[
-        THREE.MathUtils.degToRad(45),
-        THREE.MathUtils.degToRad(45),
+      <mesh 
+        ref={boxRef}
+        onClick={handleClick}
+        position={[0,3,0]}
+        rotation={[
+        THREE.MathUtils.degToRad(controls.xRotation),
+        THREE.MathUtils.degToRad(controls.yRotation),
         0]}>
         <boxGeometry />
         <meshMatcapMaterial color="hotpink" />
