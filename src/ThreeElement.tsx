69068866a975685ae9d271df96ce82db7408e2d2
Daco2020 <dacokim32@gmail.com>
2023-12-22 23:44:54 +0900

    wireFrame, geometry 공유, boxGeometry 옵션 설명

--- src/ThreeElement.tsx
+++ src/ThreeElement.tsx
@@ -1,90 +1,46 @@
 import * as THREE from 'three'
-import { useThree, useFrame } from '@react-three/fiber'
-import { useRef, useState } from 'react'
+import { useFrame } from '@react-three/fiber'
+import { useRef, useEffect } from 'react'
+import { useControls } from 'leva'
 
 export default function ThreeElement() {
   // useRef는 개체를 참조하는데 사용되는 훅
-  const [velocity, setVelocity] = useState(0);
-  const [direction, setDirection] = useState(new THREE.Vector3());
-  const {scene} = useThree();
   const boxRef = useRef<THREE.Mesh>(null!)
-  const groupRef = useRef<THREE.Group>(null!)
+  const boxCopyRef = useRef<THREE.Mesh>(null!)
+  const boxControl = useControls({
+    width : {value:1, min:0.1, max:10, step:0.1},
+    height : {value:1, min:0.1, max:10, step:0.1},
+    depth : {value:1, min:0.1, max:10, step:0.1},
+    withSegments : {value:1, min:1, max:10, step:1},
+    heightSegments : {value:1, min:1, max:10, step:1},
+    depthSegments : {value:1, min:1, max:10, step:1},
 
+  })
 
   // 개체를 움직이게 만드는 곳
-  useFrame((_, delta) => {
-    // 중력 효과
-    let newVelocity = velocity - 12.8 * delta; // 중력 가속도 적용
-    boxRef.current.position.y += newVelocity * delta;
-    boxRef.current.position.x += direction.x * delta;
-    boxRef.current.position.z += direction.z * delta;
+  useFrame((_, delta) => {});
 
-    if (boxRef.current.position.y <= 0) {
-      // 바닥에 닿으면
-      boxRef.current.position.y = 0;
-      newVelocity *= -0.5; // 반발력 적용
-
-      if (Math.abs(newVelocity) < 0.5) {
-        newVelocity = 0; // 최소 속도 도달 시 정지
-        setDirection(new THREE.Vector3(0,0,0)); // 정지 시 방향도 0으로 설정
-    }
-  }
-    setVelocity(newVelocity);
-  });
-
-  const handleClick = () => {
-    setVelocity(5); // 초기 속도 설정
-    setDirection(new THREE.Vector3(Math.random() * 2 - 1, 0, Math.random() * 2 - 1).normalize()); // 랜덤 방향 설정
-  };
-
-  // scene.rotation.x = THREE.MathUtils.degToRad(45);
-  scene.rotation.x += 0.01;
-  groupRef.current.rotation.y += 0.01;
+  // 처음 렌더링 될 때만 실행되고 배열에 담긴 값이 바뀔 때만 실행되는 훅
+  useEffect(() => {
+    boxCopyRef.current.geometry = boxRef.current.geometry
+  },[ boxControl.width, boxControl.height, boxControl.depth, boxControl.withSegments, boxControl.heightSegments, boxControl.depthSegments])
 
   return (
     <>
-      <directionalLight position={[1,5,5]}/>
-      <group
-        ref={groupRef}
+      <directionalLight position={[5,5,5]}/>
+      <mesh 
+        ref={boxRef}
         position={[0,0,0]}
-      >  
-      <mesh 
-        ref={boxRef}
-        onClick={handleClick}
-        position={[0,0,0]}
-        scale={[1,1,1]}
-        rotation={[
-          THREE.MathUtils.degToRad(0),
-          THREE.MathUtils.degToRad(0),
-          THREE.MathUtils.degToRad(0)]}>
-        <boxGeometry />
-        <meshMatcapMaterial color="hotpink" />
+        >
+        <boxGeometry args={[boxControl.width, boxControl.height, boxControl.depth, boxControl.withSegments, boxControl.heightSegments, boxControl.depthSegments]}/>
+        <meshStandardMaterial wireframe/>
       </mesh>
-      <mesh 
-        ref={boxRef}
-        onClick={handleClick}
-        position={[3,0,0]}
-        scale={[1,1,1]}
-        rotation={[
-          THREE.MathUtils.degToRad(0),
-          THREE.MathUtils.degToRad(0),
-          THREE.MathUtils.degToRad(0)]}>
-        <boxGeometry />
-        <meshMatcapMaterial color="tomato" />
+      <mesh
+      ref={boxCopyRef}
+      >
+        <meshStandardMaterial color="tomato"/>
       </mesh>
-      <mesh 
-        ref={boxRef}
-        onClick={handleClick}
-        position={[0,0,3]}
-        scale={[1,1,1]}
-        rotation={[
-          THREE.MathUtils.degToRad(0),
-          THREE.MathUtils.degToRad(0),
-          THREE.MathUtils.degToRad(0)]}>
-        <boxGeometry />
-        <meshMatcapMaterial color="orange" />
-      </mesh>
-      </group>
+
     </>
   )
 
